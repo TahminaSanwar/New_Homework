054d6391ecb40d4423480e1e4be9f039b9a9afd9
TahminaSanwar <sanwartahmina@gmail.com>
2021-10-23 23:52:09 -0400

    HW3 10232021

---
+++ src/hw3Q2variableAndMethod/Computer.java
@@ -0,0 +1,25 @@
+package hw3Q2variableAndMethod;
+
+public class Computer {
+	
+	public String Brand;
+	public String Model;
+	public String OperatingSystem;
+	public int price;
+	public char grade;
+	public boolean madeInUSA;
+	
+	
+
+	public Computer() {
+		
+	}
+	
+	public void computer() {
+		System.out.println("My Brand: "+Brand+" \n Model: "+Model+" \n Operating system: "+OperatingSystem+"\n Price: "+price+"\n Made in USA? Ans: "+madeInUSA);
+		
+	}
+	
+	
+
+}
---
+++ src/hw3Q2variableAndMethod/Selenium_java.java
@@ -0,0 +1,15 @@
+package hw3Q2variableAndMethod;
+
+public class Selenium_java {
+	public Selenium_java() {
+		System.out.println("Lobid is a good boy");
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
