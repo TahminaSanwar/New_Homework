d632e0b5c3b920c56ef1e79eb4c005ce3cbf24ae
TahminaSanwar <sanwartahmina@gmail.com>
2021-11-15 18:07:09 -0500

    HW8

--- src/hw8Q2UseOfSuperInChildClass/Daughter.java
+++ src/hw8Q2UseOfSuperInChildClass/Daughter.java
@@ -1,36 +1,67 @@
 package hw8Q2UseOfSuperInChildClass;
 
-public class Daughter extends Father{
+public class Daughter extends Father {
 	public String birthMonth;
 	public int age;
 
+	public Daughter() {
+		super("Abdur", 67, 'M', false);// Super method can call either default or parameterized constructor from a
+										// parent class
+		super.father();
+		super.fatherInfo("Razzaque", 66, 'M', false);// Super keyword can call the parameterized method from the parent
+														// class
+		super.familyName = "Molla";// super keyword can initialize variable from parent class
+        super.name = "Abdur";
+        super.age = 66;
+        super.sex = 'M';
+        super.usCitizen = false;
+		System.out.println("Family name is:" + familyName);
+		System.out.println("This is a default constructor from Daughter class");
 
-public Daughter() {
-	super("Abdur", 67, 'M', false);//Super method can call either default or parameterized constructor from a parent class
-	super.father();//Super keyword can call the default method from the parent class.
-	super.fatherInfo("Razzaque",66,'M',false);//Super keyword can call the parameterized method from the parent class
-	super.familyName = "Molla";//super keyword can initialize variable from parent class
-	System.out.println("This is a default constructor");
-	
-	
-}
+	}
 
+	public Daughter(String birthMonth, int age) {
+		super("Tahmina", 36, 'F', false);// Super method can call either default or parameterized constructor from a
+											// parent class
+		super.fatherInfo("Lima", 44, 'F', false);// Super keyword can call the parameterized method from the parent
+													// class
+		super.familyName = "Molla";// super keyword can initialize variable from parent class
+		super.name = "Abdur";
+        super.age = 66;
+        super.sex = 'M';
+        super.usCitizen = false;
+        System.out.println("Family name is:" + familyName);
 
-public Daughter(String birthMonth, int age) {
-	
-	this.birthMonth = birthMonth;
-	this.age = age;
-	System.out.println("Birth Month: "+birthMonth+"\nAge:"+age);
-}
+		this.birthMonth = birthMonth;
+		this.age = age;
 
-public void daughter() {
-}	
+		System.out.println("Birth Month: " + birthMonth + "\nAge:" + age);
+	}
 
-public void daughterInfo(String birthMonth, int age) {
+	public void daughter() {
+		super.father();
+		super.fatherInfo("Lima", 44, 'F', false);
+		super.familyName = "Molla";// super keyword can initialize variable from parent class
+		super.name = "Abdur";
+        super.age = 66;
+        super.sex = 'M';
+        super.usCitizen = false;
+        System.out.println("Family name is:" + familyName);
+		System.out.println("This is a void method from Daughter class");
+	}
 
-	this.birthMonth = birthMonth;
-	this.age = age;
-	System.out.println("Birth Month: "+birthMonth+"\nAge:"+age);
-}
-}	
+	public void daughterInfo(String birthMonth, int age) {
+		super.father();
+		super.fatherInfo("Lima", 44, 'F', false);
+		super.familyName = "Molla";// super keyword can initialize variable from parent class
+		super.name = "Abdur";
+        super.age = 66;
+        super.sex = 'M';
+        super.usCitizen = false;
+        System.out.println("Family name is:" + familyName);
+
+		this.birthMonth = birthMonth;
+		this.age = age;
+		System.out.println("Birth Month: " + birthMonth + "\nAge:" + age);
+	}
 }
--- src/hw8Q2UseOfSuperInChildClass/TestFamily.java
+++ src/hw8Q2UseOfSuperInChildClass/TestFamily.java
@@ -1,5 +1,37 @@
 package hw8Q2UseOfSuperInChildClass;
 
 public class TestFamily {
+	public static void main(String[] args) {
+		System.out.println("****Default constructor initialized for Daughter Class****");
+
+		Daughter daughter01 = new Daughter();
+
+		System.out.println("****parameterized constructor initialized for Daughter Class****");
+
+		Daughter daughter02 = new Daughter("October", 36);
+
+		System.out.println("****void type method initialized for Daughter Class****");
+
+		daughter01.daughter();
+
+		System.out.println("****Parameterized method initialized for Daughter Class****");
+
+		daughter01.daughterInfo("October", 36);
+
+		System.out.println("****Default constructor initialized for Father Class****");
+
+		Father father01 = new Father();
+
+		System.out.println("****parameterized constructor initialized for Father Class****");
+
+		Father father02 = new Father("Abdur", 67, 'M', false);
+
+		System.out.println("****void type method initialized for Father Class****");
+		father01.father();
+
+		System.out.println("****Parameterized method initialized for Father Class****");
+		father01.fatherInfo("Abdur", 67, 'M', false);
+
+	}
 
 }
