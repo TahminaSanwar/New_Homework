19eef9f39a08dfe22460f03555c5cbebc10d5c78
TahminaSanwar <sanwartahmina@gmail.com>
2021-12-14 01:20:54 -0500

    HW11

--- src/java_oop_allTogether/Iphone1.java
+++ src/java_oop_allTogether/Iphone1.java
@@ -1,6 +1,60 @@
 package java_oop_allTogether;
 
 public class Iphone1 extends AppleWatch implements Phone {
+	private int price;
+	private String info;
+	private char user;
+	private boolean madeInUsa;
+	
+	
+
+	public int getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+
+	public String getInfo() {
+		return info;
+	}
+
+
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+
+
+	public char getUser() {
+		return user;
+	}
+
+
+
+	public void setUser(char user) {
+		this.user = user;
+	}
+
+
+
+	public boolean isMadeInUsa() {
+		return madeInUsa;
+	}
+
+
+
+	public void setMadeInUsa(boolean madeInUsa) {
+		this.madeInUsa = madeInUsa;
+	}
+
+
 
 	public void regularClassInfo() {
 		System.out.println("A class contain only non abstract method. Abstract classcontain abstract and non abstract method both. In Class method is only implemented. In abstract class method is only defined (non abstract method is implemented) and in interface method is only declared. A Class can be instansiated. But an abstract class and interface can not be instansiated");
--- src/java_oop_allTogether/TestPhone.java
+++ src/java_oop_allTogether/TestPhone.java
@@ -25,6 +25,16 @@ public class TestPhone {
 		ph.message();
 		ph.camera();
 		ph.battery();
+		
+		System.out.println("**********************************************");
+
+		Iphone1 iphone1 = new Iphone1();
+		iphone1.setPrice(750);
+		iphone1.setInfo("I bought this phone in 2000");
+		iphone1.setUser('F');
+		iphone1.setMadeInUsa(false);
+		
+		System.out.println("Info:"+iphone1.getInfo()+",\nThe price was:"+iphone1.getPrice()+",\nUser Sex:"+iphone1.getUser()+",\nMade in USA:"+iphone1.isMadeInUsa());
 
 	}
 
